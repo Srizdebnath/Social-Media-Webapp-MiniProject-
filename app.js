33d29dd23f8b045bced7d554fc097eb0b0d4dffc
Sriz Debnath <srizd449@gmail.com>
2024-10-15 10:50:36 +0530

    15/10/2024

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ const postModel = require("./models/post");
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const path = require("path");
+const upload = require("./config/multerconfig");
+
 
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(cookieParser());
+
 
 app.get("/", function(req,res){
     res.render("index");
+});
+
+app.get("/profile/upload", function(req,res){
+    res.render("profileupload");
+});
+
+app.post("/upload",isLoggedIn,upload.single("image"), async function(req,res){
+    let user = await userModel.findOne({email: req.user.email});
+    user.profilepic = req.file.filename;
+    await user.save();
+    res.redirect("/profile");
 });
 
 
